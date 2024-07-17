0d0adeaffd40c7a0cbc08f1119e9021339e5ddb7
Haomin SunWu <wsun601@UoA.auckland.ac.nz>
2024-07-17 15:39:04 +1200

    Update credetials.json

---
+++ generate_token.py
@@ -0,0 +1,25 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os.path
+import pickle
+
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+CLIENT_SECRET_FILE = 'credentials.json'
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRET_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    print("Token saved to token.pickle")
+
+if __name__ == '__main__':
+    main()
